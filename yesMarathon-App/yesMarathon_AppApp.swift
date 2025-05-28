20fdd3f07c4a2e1117dc27b7b7644cca754f5aea
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-05-28 23:12:52 +0900

    Initial Commit

---
+++ yesMarathon-App/yesMarathon_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  yesMarathon_AppApp.swift
+//  yesMarathon-App
+//
+//  Created by A S on 2025/05/28.
+//
+
+import SwiftUI
+
+@main
+struct yesMarathon_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ yesMarathon-AppTests/yesMarathon_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  yesMarathon_AppTests.swift
+//  yesMarathon-AppTests
+//
+//  Created by A S on 2025/05/28.
+//
+
+import Testing
+@testable import yesMarathon_App
+
+struct yesMarathon_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
