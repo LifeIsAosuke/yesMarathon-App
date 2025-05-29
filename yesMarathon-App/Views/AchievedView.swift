65622e95889de91e2ad7f95fc30fa93097e82f25
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-05-29 12:07:52 +0900

    ボタンを押すとAchievedViewにナビゲーション遷移するように修正

--- yesMarathon-App/Views/AchievedView.swift
+++ yesMarathon-App/Views/AchievedView.swift
@@ -24,6 +24,7 @@ struct AchievedView: View {
                 .padding()
         }
         .padding()
+        .navigationBarBackButtonHidden(true)
     }
 }
 
--- yesMarathon-App/Views/HomeView.swift
+++ yesMarathon-App/Views/HomeView.swift
@@ -20,56 +20,61 @@ struct HomeView: View {
     
     var body: some View {
         VStack {
-            Text("本日のYES")
-                .foregroundColor(Color(.sRGB, red: 153 / 255.0, green: 153 / 255.0, blue: 153 / 255.0))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
-            
-            //お題ラベル
-            HStack {
-                Text(yesLabel)
-                    .font(.title)
-                    .fontWeight(.bold)
+            NavigationStack {
+                Text("本日のYES")
+                    .foregroundColor(Color(.sRGB, red: 153 / 255.0, green: 153 / 255.0, blue: 153 / 255.0))
+                    .frame(maxWidth: .infinity, alignment: .leading)
                     .padding()
-                    .frame(width: 268, height: 180, alignment: .leading)
                 
-                Button(action: {
+                //お題ラベル
+                HStack {
+                    Text(yesLabel)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding()
+                        .frame(width: 268, height: 180, alignment: .leading)
                     
-                    yesLabel = yesSuggestion.random()
-                }) {
-                    VStack {
-                        ZStack {
-                            Circle()
-                                .foregroundColor(Color(red: 217 / 255.0, green: 217 / 255.0, blue: 217 / 255.0))
-                                .opacity(80.5)
-                                .frame(width: 44, height: 44)
-                            Image(systemName: "arrow.trianglehead.2.clockwise")
+                    Button(action: {
+                        
+                        yesLabel = yesSuggestion.random()
+                    }) {
+                        VStack {
+                            ZStack {
+                                Circle()
+                                    .foregroundColor(Color(red: 217 / 255.0, green: 217 / 255.0, blue: 217 / 255.0))
+                                    .opacity(80.5)
+                                    .frame(width: 44, height: 44)
+                                Image(systemName: "arrow.trianglehead.2.clockwise")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 24))
+                            }
+                            Text("シャッフル")
                                 .foregroundColor(.black)
-                                .font(.system(size: 24))
+                                .font(.system(size: 10))
                         }
-                        Text("シャッフル")
-                            .foregroundColor(.black)
-                            .font(.system(size: 10))
+                    }
+                   
+                }
+                Spacer()
+                // YESボタン
+                NavigationLink {
+                    AchievedView()
+                } label: {
+                    ZStack {
+                        Circle()
+                            .foregroundColor(Color(red: 255 / 255.0, green: 161 / 255.0, blue: 0 / 255.0))
+                            .frame(width: 320, height: 320)
+                        Circle()
+                            .foregroundColor(Color(red: 255 / 255.0, green: 123 / 255.0, blue: 0 / 255.0))
+                            .frame(width: 310, height: 310)
+                        Text("YES!")
+                            .font(.system(size: 90))
+                            .foregroundColor(.white)
                     }
                 }
-                .padding()
+
+                Spacer()
             }
-            Spacer()
-            // YESボタン
-            Button(action: {}) {
-                ZStack {
-                    Circle()
-                        .foregroundColor(Color(red: 255 / 255.0, green: 161 / 255.0, blue: 0 / 255.0))
-                        .frame(width: 320, height: 320)
-                    Circle()
-                        .foregroundColor(Color(red: 255 / 255.0, green: 123 / 255.0, blue: 0 / 255.0))
-                        .frame(width: 310, height: 310)
-                    Text("YES!")
-                        .font(.system(size: 90))
-                        .foregroundColor(.white)
-                }
-            }
-            Spacer()
         }
         .padding()
         
