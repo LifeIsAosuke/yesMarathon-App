8c3818bf39812bc7733947b1c917ae3843d0b5d0
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-05-30 23:59:55 +0900

    HomeViewからYesLogViewへのナビゲーション遷移を実装

---
+++ yesMarathon-App/Views/YesLogView.swift
@@ -0,0 +1,34 @@
+//
+//  YesLogView.swift
+//  yesMarathon-App
+//
+//  Created by A S on 2025/05/30.
+//
+
+import SwiftUI
+
+struct YesLogView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Text("ホーム画面へ")
+                }
+                .foregroundColor(.black)
+            }
+        }
+    }
+}
+
+#Preview {
+    YesLogView()
+}
