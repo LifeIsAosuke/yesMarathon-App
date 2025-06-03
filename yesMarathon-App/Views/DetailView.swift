4357cd5ae5a5306e3ccfa27ebfa81f3f76898aee
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-06-03 23:43:55 +0900

    YES評価の編集画面まで作成

--- yesMarathon-App/Views/DetailView.swift
+++ yesMarathon-App/Views/DetailView.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import SwiftData
+import PhotosUI
 
 struct DetailView: View {
     @Environment(\.modelContext) private var modelContext
     @State var matchingData: EachDayData
     
     @Environment(\.dismiss) private var dismiss
+    
+    // 編集画面を管理する変数
+    @State var isEditing: Bool = false
+    
+    //---編集中に使う変数---
+    @State private var editingComent: String = ""
+    @State private var stars: [Int] = [1, 1, 1, 0, 0]
+    @State private var yesEvaluation: Int = 3
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var selectedImage: Data?
 
     // 取得したカレンダーのフォーマットを指定
     private let dateFormatter: DateFormatter = {
@@ -44,9 +56,14 @@ struct DetailView: View {
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding()
 
-                    Text(matchingData.comment)
-                        .frame(height: 40)
-                        .padding()
+                    if !isEditing {
+                        Text(matchingData.comment)
+                            .frame(height: 40)
+                            .padding()
+                    } else { // 編集画面
+                        TextField("\(matchingData.comment)", text: $editingComent)
+                            .padding()
+                    }
                 }
 
                 Divider()
@@ -60,20 +77,51 @@ struct DetailView: View {
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding()
                     
-                    HStack {
-                        // 色塗りスターの表示
-                        ForEach(0..<matchingData.yesEvaluation, id: \.self) { _ in
-                            Image(systemName: "star.fill")
-                                .foregroundColor(Color(red: 253 / 255.0, green: 202 / 255.0, blue: 0 / 255.0))
+                    if !isEditing {
+                        HStack {
+                            // 色塗りスターの表示
+                            ForEach(0..<matchingData.yesEvaluation, id: \.self) { _ in
+                                Image(systemName: "star.fill")
+                                    .foregroundColor(Color(red: 253 / 255.0, green: 202 / 255.0, blue: 0 / 255.0))
+                            }
+                            
+                            // 外縁スターの表示
+                            ForEach(0..<5-matchingData.yesEvaluation, id: \.self) { _ in
+                                Image(systemName: "star")
+                            }
                         }
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .center)
                         
-                        // 外縁スターの表示
-                        ForEach(0..<5-matchingData.yesEvaluation, id: \.self) { _ in
-                            Image(systemName: "star")
+                    } else { // 編集画面
+                        HStack(alignment: .top) {
+                            ForEach(0..<5) { index in
+                                Button(action: {
+                                    for i in 0...index {
+                                        stars[i] = 1
+                                    }
+                                    for i in (index+1)..<5 {
+                                        stars[i] = 0
+                                    }
+                                    yesEvaluation = index + 1
+                                }) {
+                                    VStack {
+                                        Image(systemName: stars[index] == 1 ? "star.fill" : "star")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 24, height: 24)
+                                            .foregroundColor(stars[index] == 1 ? Color.yesYellow : .gray)
+                                        if index == 0 { Text("イマイチ").font(.caption) }
+                                        if index == 2 { Text("イイネ!").font(.caption) }
+                                        if index == 4 { Text("バチイケ!!").font(.caption) }
+                                    }
+                                    .foregroundColor(.black)
+                                }
+                            }
                         }
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .center)
                     }
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .center)
                 }
 
                 Divider()
@@ -127,6 +175,24 @@ struct DetailView: View {
                         }
                     }
                     .foregroundColor(.black)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    if !isEditing{
+                        Button {
+                            isEditing.toggle()
+                        } label: {
+                            Text("編集")
+                                .foregroundColor(.black)
+                        }
+                    } else {
+                        Button {
+                            isEditing.toggle()
+                        } label: {
+                            Text("完了")
+                                .foregroundColor(.black)
+                                .bold()
+                        }
+                    }
                 }
             }
         }
