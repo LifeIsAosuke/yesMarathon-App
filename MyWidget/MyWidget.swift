f967083eca1109066d8dde904fb593db2aa2a8a4
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-06-06 13:44:23 +0900

    widgetのパーツ配置

--- MyWidget/MyWidget.swift
+++ MyWidget/MyWidget.swift
@@ -8,54 +8,100 @@
 import WidgetKit
 import SwiftUI
 
+// ウィジェットに表示するデータ構造
+struct yesData: Identifiable {
+    var id: UUID
+    var yesLabel: String
+    var achievedCount: Int
+}
+
+// ここのデータは実際はyesMarathonAppから持ってくる
+let sampleData: yesData = yesData(id: UUID(), yesLabel: "自分らしくないことをやってみる", achievedCount: 13)
+
+
 struct Provider: TimelineProvider {
-    // widgetの初期化に読み込まれる
+    // widgetの初読み込み時やデータの取得中の時に表示される
     func placeholder(in context: Context) -> SimpleEntry {
         // 初期化時に酔い込まれるデータ
-        SimpleEntry(date: Date(), emoji: "😀")
+        SimpleEntry(date: .now, yesData: sampleData)
     }
-
+    
+    // widget追加時に表示するサンプルデータを生成
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        let entry = SimpleEntry(date: .now, yesData: sampleData)
         completion(entry)
     }
-
+    
+    // タイムラインのデータを生成
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            let entry = SimpleEntry(date: .now, yesData: sampleData)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
-
-//    func relevances() async -> WidgetRelevances<Void> {
-//        // Generate a list containing the contexts this widget is relevant in.
-//    }
+    
+    //    func relevances() async -> WidgetRelevances<Void> {
+    //        // Generate a list containing the contexts this widget is relevant in.
+    //    }
 }
 
 // ウィジェットに表示するためのデータの一つの状態
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let emoji: String
+    let yesData: yesData
 }
 
+// ウィジェットの見た目
 struct MyWidgetEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Emoji:")
-            Text(entry.emoji)
+        ZStack {
+            
+            Text("YESマラソン")
+                .font(.caption)
+                .foregroundColor(.white)
+                .bold()
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            
+            HStack {
+                ZStack {
+                    
+                    Circle()
+                        .frame(width: 60)
+                        .foregroundColor(.white)
+                        .border(Color.black, width: 2)
+                        .cornerRadius(10)
+                    
+                    Image("achievedIcon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 60)
+                        .clipShape(Circle())
+                }
+                
+                VStack() {
+                    HStack {
+                        Text("\(sampleData.achievedCount)")
+                        Text("日目 🔥")
+                    }
+                    Text("本日のYES")
+                        .frame(width: .infinity, alignment: .leading)
+                        .font(.system(size: 10))
+                        .foregroundColor(.white)
+                        .bold()
+                    Text(sampleData.yesLabel)
+                }
+                .padding()
+            }
         }
     }
 }
@@ -63,27 +109,28 @@ struct MyWidgetEntryView : View {
 // ウィジェット本体
 struct MyWidget: Widget {
     let kind: String = "MyWidget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             if #available(iOS 17.0, *) {
                 MyWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
+                //                    .containerBackground(.fill.tertiary, for: .widget)
+                    .containerBackground(Color.yesOrange, for: .widget)
             } else {
                 MyWidgetEntryView(entry: entry)
                     .padding()
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("YESマラソン")
+        .description("本日のYESと連続達成日を表示します")
     }
 }
 
-// ウィジェットの最小サイズを指定
-#Preview(as: .systemSmall) {
+
+#Preview(as: .systemMedium) { // widgetのサイズを指定
     MyWidget()
 } timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
+    SimpleEntry(date: .now, yesData: sampleData)
 }
+
